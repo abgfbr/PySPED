fc707617710f44f8a3dd65846d1666606ab74a16
Wagner Pereira <wagner.pereira@abgf.gov.br>
2018-07-10 13:26:46 -0300

    [ADD] Adicionado registro S-1299

--- pysped/esocial/leiaute/__init__.py
+++ pysped/esocial/leiaute/__init__.py
@@ -191,3 +191,6 @@ from .evtPgtos_20402 import DetRubrFer as S1210_DetRubrFer_2
 from .evtPgtos_20402 import DetPgtoAnt as S1210_DetPgtoAnt_2
 from .evtPgtos_20402 import IdePgtoExt as S1210_IdePgtoExt_2
 from .evtPgtos_20402 import InfoPgtoAnt as S1210_InfoPgtoAnt_2
+
+# S-1299
+from .evtFechaEvPer_20402 import S1299 as S1299_2
