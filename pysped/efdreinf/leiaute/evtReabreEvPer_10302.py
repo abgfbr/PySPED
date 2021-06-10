03491d8fc8c58a6fc9e9ff7cf82234f58f09a756
Luiz Felipe do Divino <lf.divino@gmail.com>
2021-06-10 14:35:50 -0300

    [FIX] Nome referencia do XSD de reabertura de evento

--- pysped/efdreinf/leiaute/evtReabreEvPer_10302.py
+++ pysped/efdreinf/leiaute/evtReabreEvPer_10302.py
@@ -158,7 +158,7 @@ class R2098(XMLNFe):
         self.evtReabreEvPer = EvtReabreEvPer()
         self.caminho_esquema = os.path.join(DIRNAME, 'schema/',
                                             ESQUEMA_ATUAL + '/')
-        self.arquivo_esquema = 'evtReabreEvPer-v1_05_01.xsd'
+        self.arquivo_esquema = 'evtReabreEvPer.xsd'
         self.id_evento = ''
         self.Signature = Signature()
         self.evento = self.evtReabreEvPer
