96251555c9f7a35ccb20220fdc2aa5ea242c6e09
Luiz Felipe do Divino <lf.divino@gmail.com>
2021-11-08 09:43:32 -0300

    [FIX] Atualização S-1299 para o novo layout V-1.0

--- pysped/esocial/leiaute/evtFechaEvPer_20402.py
+++ pysped/esocial/leiaute/evtFechaEvPer_20402.py
@@ -56,63 +56,31 @@ class InfoFech(XMLNFe):
     def __init__(self):
         super(InfoFech, self).__init__()
         self.evtRemun        = TagCaracter(nome='evtRemun',        tamanho=[1, 1], raiz='//eSocial/evrFechaEvPer/infoFech', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
-        self.evtPgtos        = TagCaracter(nome='evtPgtos',        tamanho=[1, 1], raiz='//eSocial/evrFechaEvPer/infoFech', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
-        self.evtAqProd       = TagCaracter(nome='evtAqProd',       tamanho=[1, 1], raiz='//eSocial/evrFechaEvPer/infoFech', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
         self.evtComProd      = TagCaracter(nome='evtComProd',      tamanho=[1, 1], raiz='//eSocial/evrFechaEvPer/infoFech', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
         self.evtContratAvNP  = TagCaracter(nome='evtContratAvNP',  tamanho=[1, 1], raiz='//eSocial/evrFechaEvPer/infoFech', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
         self.evtInfoComplPer = TagCaracter(nome='evtInfoComplPer', tamanho=[1, 1], raiz='//eSocial/evrFechaEvPer/infoFech', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
-        self.compSemMovto    = TagCaracter(nome='compSemMovto',    tamanho=[1, 1], raiz='//eSocial/evrFechaEvPer/infoFech', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False, obrigatorio=False)
+        self.indExcApur1250  = TagCaracter(nome='indExcApur1250',    tamanho=[1, 1], raiz='//eSocial/evrFechaEvPer/infoFech', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False, obrigatorio=False)
+        self.transDCTFWeb  = TagCaracter(nome='transDCTFWeb',    tamanho=[1, 1], raiz='//eSocial/evrFechaEvPer/infoFech', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False, obrigatorio=False)
 
     def get_xml(self):
         xml = XMLNFe.get_xml(self)
         xml += '<infoFech>'
         xml += self.evtRemun.xml
-        xml += self.evtPgtos.xml
-        xml += self.evtAqProd.xml
         xml += self.evtComProd.xml
         xml += self.evtContratAvNP.xml
         xml += self.evtInfoComplPer.xml
-        xml += self.compSemMovto.xml
+        xml += self.indExcApur1250.xml
+        xml += self.transDCTFWeb.xml
         xml += '</infoFech>'
         return xml
 
     def set_xml(self, arquivo):
         if self._le_xml(arquivo):
             self.evtRemun.xml = arquivo
-            self.evtPgtos.xml = arquivo
-            self.evtAqProd.xml = arquivo
             self.evtComProd.xml = arquivo
             self.evtContratAvNP.xml = arquivo
             self.evtInfoComplPer.xml = arquivo
             self.compSemMovto.xml = arquivo
-
-    xml = property(get_xml, set_xml)
-
-
-class IdeRespInf(XMLNFe):
-    def __init__(self):
-        super(IdeRespInf, self).__init__()
-        self.nmResp    = TagCaracter(nome='nmResp',   tamanho=[1, 60], raiz='//eSocial/evtFechaEvPer/ideRespInf', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
-        self.cpfResp   = TagCaracter(nome='cpfResp',  tamanho=[1, 11], raiz='//eSocial/evtFechaEvPer/ideRespInf', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
-        self.telefone  = TagCaracter(nome='telefone', tamanho=[1, 13], raiz='//eSocial/evtFechaEvPer/ideRespInf', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
-        self.email     = TagCaracter(nome='email',    tamanho=[1, 60], raiz='//eSocial/evtFechaEvPer/ideRespInf', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False, obrigatorio=False)
-
-    def get_xml(self):
-        xml = XMLNFe.get_xml(self)
-        xml += '<ideRespInf>'
-        xml += self.nmResp.xml
-        xml += self.cpfResp.xml
-        xml += self.telefone.xml
-        xml += self.email.xml
-        xml += '</ideRespInf>'
-        return xml
-
-    def set_xml(self, arquivo):
-        if self._le_xml(arquivo):
-            self.nmResp.xml = arquivo
-            self.cpfResp.xml = arquivo
-            self.telefone.xml = arquivo
-            self.email.xml = arquivo
 
     xml = property(get_xml, set_xml)
 
@@ -144,8 +112,9 @@ class IdeEvento(XMLNFe):
         super(IdeEvento, self).__init__()
         self.indApuracao = TagCaracter(nome='indApuracao', tamanho=[1,  1], raiz='//eSocial/evtFechaEvPer/ideEvento', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
         self.perApur     = TagCaracter(nome='perApur',     tamanho=[1,  7], raiz='//eSocial/evtFechaEvPer/ideEvento', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
+        self.indGuia = TagInteiro(nome='indGuia', tamanho=[1, 1], raiz='//eSocial/evtFechaEvPer/ideEvento', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False, obrigatorio=False)
         self.tpAmb       = TagInteiro( nome='tpAmb',       tamanho=[1,  1], raiz='//eSocial/evtFechaEvPer/ideEvento', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False, valor=2)
-        self.procEmi     = TagInteiro( nome='procEmi',     tamanho=[1,  1], raiz='//eSocial/evtFechaEvPer/ideEvento', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False, valor=1)
+        self.procEmi     = TagInteiro( nome='procEmi',     tamanho=[1,  2], raiz='//eSocial/evtFechaEvPer/ideEvento', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False, valor=1)
         self.verProc     = TagCaracter(nome='verProc',     tamanho=[1, 20], raiz='//eSocial/evtFechaEvPer/ideEvento', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
 
     def get_xml(self):
@@ -153,6 +122,7 @@ class IdeEvento(XMLNFe):
         xml += '<ideEvento>'
         xml += self.indApuracao.xml
         xml += self.perApur.xml
+        xml += self.indGuia.xml
         xml += self.tpAmb.xml
         xml += self.procEmi.xml
         xml += self.verProc.xml
@@ -163,6 +133,7 @@ class IdeEvento(XMLNFe):
         if self._le_xml(arquivo):
             self.indApuracao.xml = arquivo
             self.perApur.xml = arquivo
+            self.indGuia.xml = arquivo
             self.tpAmb.xml = arquivo
             self.procEmi.xml = arquivo
             self.verProc.xml = arquivo
@@ -176,7 +147,6 @@ class EvtFechaEvPer(XMLNFe):
         self.Id = TagCaracter(nome='evtFechaEvPer', propriedade='Id', raiz='//eSocial/evtFechaEvPer', namespace=NAMESPACE_ESOCIAL, namespace_obrigatorio=False)
         self.ideEvento = IdeEvento()
         self.ideEmpregador = IdeEmpregador()
-        self.ideRespInf = IdeRespInf()
         self.infoFech = InfoFech()
 
     def get_xml(self):
@@ -184,7 +154,6 @@ class EvtFechaEvPer(XMLNFe):
         xml += self.Id.xml
         xml += self.ideEvento.xml
         xml += self.ideEmpregador.xml
-        xml += self.ideRespInf.xml
         xml += self.infoFech.xml
         xml += '</evtFechaEvPer>'
         return xml
