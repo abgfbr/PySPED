c293570783ea0983f185b1e6936343b7788b694e
Luiz Felipe do Divino <lf.divino@gmail.com>
2023-03-29 15:46:21 -0300

    [FIX] Mudança de v-1.0 para v-1.1

--- pysped/esocial/leiaute/__init__.py
+++ pysped/esocial/leiaute/__init__.py
@@ -44,7 +44,7 @@ from __future__ import division, print_function, unicode_literals
 #
 # Versão 2.05.00
 #
-ESQUEMA_ATUAL_VERSAO_2 = 'v1.00.00'
+ESQUEMA_ATUAL_VERSAO_2 = 'v1.01.00'
 
 from .soap_10100 import SOAPEnvio as SOAPEnvio_10100
 from .soap_10100 import SOAPRetorno as SOAPRetorno_10100
